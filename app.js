06f320ba49c56ef2ae44a56dd7d3d5c97acffaa8
Andrew Liu <andrew.liu@entrustdatacard.com>
2016-08-25 12:53:27 -0400

    Send createPage message to server

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.post('/createPage', function (req, res) {
+  res.send('Server received "createPage" message."');
+});
+
 app.listen(3000, function () {
   console.log("App listening on port 3000!");
 });
