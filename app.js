e05231048ea9dbdae5b96e0bfb012227db2b5b75
Andrew Liu <andrew.liu@entrustdatacard.com>
2016-08-25 15:02:59 -0400

    Generate random id

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var app = express();
 app.use(express.static('public'));
 
 app.post('/create', function (req, res) {
+  var id = generateId();
   // Create unique id
   // Add id to list of ids in table
   // Create dummy data
   // Redirect to ?id=foo
-  res.redirect('/foo');
+  res.redirect('/' + id);
 });
 
 app.get('/:id', function (req, res) {
@@ -20,3 +21,14 @@ app.get('/:id', function (req, res) {
 app.listen(3000, function () {
   console.log("App listening on port 3000!");
 });
+
+function generateId() {
+  const CHARACTERS = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  var id = '';
+  var i;
+  for (i = 0; i < 16; i++) {
+    var randomIndex = Math.floor(Math.random() * CHARACTERS.length);
+    id += CHARACTERS[randomIndex];
+  }
+  return id;
+}
