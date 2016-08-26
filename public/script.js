863138c49fe950d775fd80c50aaec046d9e93867
Andrew Liu <andrew.liu@entrustdatacard.com>
2016-08-26 15:29:32 -0400

    Escape to cancel edit, put cursor at the end

--- public/script.js
+++ public/script.js
@@ -1,16 +1,25 @@
 var taskName = document.getElementById('task-name');
 var taskNameEditor = document.getElementById('task-name-editor');
 
-
-function showTaskNameEditor() {
+taskName.onclick = function (event) {
   taskName.style.display = 'none';
   taskNameEditor.style.display = 'block';
+  taskNameEditor.focus();
+
+  // Put cursor at the end
+  var value = taskNameEditor.value;
+  taskNameEditor.setSelectionRange(value.length, value.length);
 }
 
-taskNameEditor.onkeypress = function (event) {
-  if (event.keyCode === 13) {
+taskNameEditor.onkeydown = function (event) {
+  console.log(event.key);
+  if (event.key === 'Enter') {
     taskNameEditor.style.display = 'none';
     taskName.textContent = taskNameEditor.value;
+    taskName.style.display = 'block';
+  }
+  if (event.key === 'Escape') {
+    taskNameEditor.style.display = 'none';
     taskName.style.display = 'block';
   }
 };
